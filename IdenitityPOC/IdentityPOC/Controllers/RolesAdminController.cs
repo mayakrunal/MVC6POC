039813e4c0830c87c42621db734904a9846dc0e2
Krunal Parekh <maya.krunal@gmail.com>
2016-12-08 17:47:00 +0800

    simple forms authenticatoin sample

---
+++ IdenitityPOC/IdentityPOC/Controllers/RolesAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityPOC.Controllers
+{
+    public class RolesAdminController : Controller
+    {
+        // GET: RolesAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCFormsAuthentication/MVCFormsAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using MVCFormsAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCFormsAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (model.UserName == "admin" && model.Password == "password")
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
