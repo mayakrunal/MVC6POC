c890c0b50dbef6e764481479c900d826e60c44b3
Krunal Parekh <maya.krunal@gmail.com>
2017-03-03 16:41:24 +0800

    added party invite

---
+++ BookStudy/PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
--- UnitTesting/test/PrimeService.Tests/Tests.cs
+++ UnitTesting/test/PrimeService.Tests/Tests.cs
@@ -6,10 +6,10 @@ namespace Prime.UnitTests.Services
     public class PrimeService_IsPrimeShould
     {
         private readonly PrimeService _primeService;
-         public PrimeService_IsPrimeShould()
-         {
-             _primeService = new PrimeService();
-         }
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
 
         [Theory]
         [InlineData(-1)]
@@ -22,28 +22,28 @@ namespace Prime.UnitTests.Services
             Assert.False(result, $"{value} should not be prime");
         }
 
-        [Theory] 
-        [InlineData(2)] 
-        [InlineData(3)] 
-        [InlineData(5)] 
-        [InlineData(7)] 
-        public void ReturnTrueGivenPrimesLessThan10(int value) 
-        { 
-            var result = _primeService.IsPrime(value); 
- 
-            Assert.True(result, $"{value} should be prime"); 
-        } 
- 
-        [Theory] 
-        [InlineData(4)] 
-        [InlineData(6)] 
-        [InlineData(8)] 
-        [InlineData(9)] 
-        public void ReturnFalseGivenNonPrimesLessThan10(int value) 
-        { 
-            var result = _primeService.IsPrime(value); 
- 
-            Assert.False(result, $"{value} should not be prime"); 
-        } 
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void ReturnTrueGivenPrimesLessThan10(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void ReturnFalseGivenNonPrimesLessThan10(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
     }
 }
