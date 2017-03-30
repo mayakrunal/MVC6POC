ff6cde1afd2e7adf71ae8f9be9445770031dfda0
Krunal <Krunal@KLAPTOP>
2017-03-31 00:31:34 +0800

    added vscode party invite

---
+++ VSCode POC/PartyInvite/Migrations/20170329145847_Initial.Designer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PartyInvites.Models;
+
+namespace PartyInvite.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20170329145847_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("PartyInvites.Models.GuestResponse", b =>
+                {
+                    b.Property<int>("id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email")
+                        .IsRequired();
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.Property<string>("Phone")
+                        .IsRequired();
+
+                    b.Property<bool?>("WillAttend")
+                        .IsRequired();
+
+                    b.HasKey("id");
+
+                    b.ToTable("Invites");
+                });
+        }
+    }
+}
--- VSCode POC/PartyInvite/Startup.cs
+++ VSCode POC/PartyInvite/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
 
 namespace PartyInvite
 {
@@ -16,6 +17,7 @@ namespace PartyInvite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IRepository, EFRepository>();
             services.AddMvc();
         }
 
