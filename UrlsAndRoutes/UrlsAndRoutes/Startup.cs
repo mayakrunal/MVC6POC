3d50ddfb31fccd31311248ae001d947682ef7dee
Krunal <Krunal@KLAPTOP>
2017-04-30 18:11:25 +0800

    urls & routes

---
+++ UrlsAndRoutes/UrlsAndRoutes/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using UrlsAndRoutes.Infrastructure;
+
+namespace UrlsAndRoutes
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<RouteOptions>(options =>
+            {
+                options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint));
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseStatusCodePages();
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                //  routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id:weekday?}");
+            });
+
+            // app.UseMvcWithDefaultRoute();
+        }
+    }
+}
