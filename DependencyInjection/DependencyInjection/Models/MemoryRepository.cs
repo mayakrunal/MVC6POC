2377be81404e10df40b70d1ff669242066a09701
Krunal Parekh <maya.krunal@gmail.com>
2017-05-02 16:08:20 +0800

    dependancy injection

---
+++ DependencyInjection/DependencyInjection/Models/MemoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public class MemoryRepository : IRepository
+    {
+        private IModelStorage storage;
+
+        private string guid = System.Guid.NewGuid().ToString();
+
+        public MemoryRepository(IModelStorage modelStore)
+        {
+            storage = modelStore;
+            new Dictionary<string, Product>();
+                new List<Product> {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M }
+                }.ForEach(p => AddProduct(p));
+        }
+        public Product this[string name] => storage[name];
+
+        public IEnumerable<Product> Products => storage.Items;
+
+        public void AddProduct(Product product) => storage[product.Name] = product;
+
+        public void DeleteProduct(Product product) => storage.RemoveItem(product.Name);
+
+        public override string ToString()
+        {
+            return guid;
+        }
+    }
+}
---
+++ DependencyInjection/DependencyInjection/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using DependencyInjection.Models;
+
+namespace DependencyInjection
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IModelStorage, DictionaryStorage>();
+            services.AddSingleton<IRepository, MemoryRepository>();
+            services.AddTransient<ProductTotalizer>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseStatusCodePages();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
