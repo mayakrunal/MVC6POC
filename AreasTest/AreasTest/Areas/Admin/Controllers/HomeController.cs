10e729c8ef58160baa0de76895e2b99bd1b5ba92
Krunal <Krunal@KLAPTOP>
2017-05-09 20:26:06 +0800

    area test

---
+++ AreasTest/AreasTest/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AreasTest.Areas.Admin.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AreasTest.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private Person[] data = new Person[] {
+            new Person { Name = "Alice", City = "London" },
+            new Person { Name = "Bob", City = "Paris" },
+            new Person { Name = "Joe", City = "New York" }
+            };
+
+        // GET: /<controller>/
+        public IActionResult Index() => View(data);
+    }
+}
